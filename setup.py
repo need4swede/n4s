a9370f0af522257d5322c50c2721338623239058
Need4Swede <theneed4swede@gmail.com>
2022-07-03 15:55:04 -0700

    1.52

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='n4s',
-    version='1.51',
+    version='1.52',
     author='Mike Afshari',
     author_email='theneed4swede@gmail.com',
     description='Collection of useful methods by Need4Swede',
