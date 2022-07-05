632911a65acf975e2efebdc6e90516a2881e5702
Need4Swede <theneed4swede@gmail.com>
2022-07-05 11:00:09 -0700

    added wait and pause to terminal

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='n4s',
-    version='2.1.5',
+    version='2.1.6',
     author='Mike Afshari',
     author_email='theneed4swede@gmail.com',
     description='Collection of useful methods by Need4Swede',
