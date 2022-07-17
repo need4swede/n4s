9939f1cf28864b033c87ce3480fd49035c3775dc
Need4Swede <theneed4swede@gmail.com>
2022-07-17 16:51:04 -0700

    added README

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import setuptools
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setuptools.setup(
     name='n4s',
     version='2.2.5',
     author='Mike Afshari',
     author_email='theneed4swede@gmail.com',
     description='Collection of useful methods by Need4Swede',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/n4s/n4s',
     license='MIT',
     packages=setuptools.find_packages(),
