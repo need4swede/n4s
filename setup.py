4c4fcfb74ccaf78719c0e7dec2cf852057f0ee48
Need4Swede <theneed4swede@gmail.com>
2022-07-19 11:34:44 -0700

    added requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 import setuptools
 
+# for pip >= 10
+from pip._internal.req import parse_requirements
+
+def load_requirements(fname):
+    reqs = parse_requirements(fname, session="test")
+    return [str(ir.req) for ir in reqs]
+
 with open('README.md') as f:
     long_description = f.read()
 
@@ -13,6 +20,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/n4s/n4s',
     license='MIT',
+    install_requires=load_requirements("requirements.txt"),
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
