80423f8f5b261d329fc957fd3affb9019d0eadc6
Need4Swede <theneed4swede@gmail.com>
2022-08-02 10:05:41 -0700

    2.2.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setuptools.setup(
     name='n4s',
-    version='2.2.6',
+    version='2.2.7',
     author='Mike Afshari',
     author_email='theneed4swede@gmail.com',
     description='Collection of useful methods by Need4Swede',
