725d7cab0d93b80fe4b5d6a8d079dddfc325c4b6
Need4Swede <theneed4swede@gmail.com>
2022-08-12 15:36:09 -0700

    removed build and egg info

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.md') as f:
 
 if platform.system() == "Darwin":
     req_list = ["appscript==1.2.0", "beautifulsoup4==4.11.1", "requests==2.27.1"]
-    # install_packages = setuptools.find_packages()
+    install_packages = setuptools.find_packages()
 else:
     req_list = ["beautifulsoup4==4.11.1", "requests==2.27.1"]
-    # install_packages = setuptools.find_packages(exclude=["appscript==1.2.0"])
+    install_packages = setuptools.find_packages(exclude=["appscript==1.2.0"])
 
 setuptools.setup(
     name='n4s',
-    version='2.3.3',
+    version='2.3.4',
     author='Mike Afshari',
     author_email='theneed4swede@gmail.com',
     description='Collection of useful methods by Need4Swede',
@@ -21,7 +21,7 @@ setuptools.setup(
     url='https://github.com/n4s/n4s',
     license='MIT',
     install_requires=req_list,
-    packages=req_list,
+    packages=install_packages,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
