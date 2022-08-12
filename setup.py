b68be9bf3116e8278b6280eabafe3562d34c404b
Need4Swede <theneed4swede@gmail.com>
2022-08-12 14:57:06 -0700

    made find packages exclusions into a list

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ if platform.system() == "Darwin":
     install_packages = setuptools.find_packages()
 else:
     req_list = ["beautifulsoup4==4.11.1", "requests==2.27.1"]
-    install_packages = setuptools.find_packages(exclude="appscript==1.2.0")
+    install_packages = setuptools.find_packages(exclude=["appscript==1.2.0"])
 
 setuptools.setup(
     name='n4s',
-    version='2.3.1',
+    version='2.3.2',
     author='Mike Afshari',
     author_email='theneed4swede@gmail.com',
     description='Collection of useful methods by Need4Swede',
