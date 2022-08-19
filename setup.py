84dd2466b534d3701260b489540b245fa218a60d
Need4Swede <theneed4swede@gmail.com>
2022-08-19 14:13:12 -0700

    2.3.7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ pip(['install', '--user', 'requests==2.27.1'])
 
 setuptools.setup(
     name='n4s',
-    version='2.3.6',
+    version='2.3.7',
     author='Mike Afshari',
     author_email='theneed4swede@gmail.com',
     description='Collection of useful methods by Need4Swede',
